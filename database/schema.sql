b066e6db7e51bda301070a4119fe6e939a8a3a1e
Raushan Kumar <raushanmansagar12345@gmail.com>
2025-02-25 18:44:46 +0530

    database

--- database/schema.sql
+++ database/schema.sql
@@ -8,3 +8,52 @@ CREATE TABLE users (
     email VARCHAR(100) UNIQUE NOT NULL,
     password_hash VARCHAR(255) NOT NULL
 );
+
+CREATE TABLE feedback (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL,
+    message TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE recipes (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    image TEXT,
+    description TEXT NOT NULL,
+    servings INT,
+    prep_time VARCHAR(50),
+    cook_time VARCHAR(50),
+    ready_time VARCHAR(50),
+    category VARCHAR(100),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE ingredients (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    recipe_id INT,
+    ingredient VARCHAR(255) NOT NULL,
+    metric VARCHAR(100),
+    us_measure VARCHAR(100),
+    FOREIGN KEY (recipe_id) REFERENCES recipes(id) ON DELETE CASCADE
+);
+
+CREATE TABLE directions (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    recipe_id INT,
+    step_number INT,
+    instruction TEXT NOT NULL,
+    FOREIGN KEY (recipe_id) REFERENCES recipes(id) ON DELETE CASCADE
+);
+
+
+
+
+CREATE TABLE saved_recipes (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    recipe_id INT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY (recipe_id) REFERENCES recipes(id) ON DELETE CASCADE
+);
